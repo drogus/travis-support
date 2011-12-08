4565009ec1d89b146acf78ad2c6bf3e5b211cd11
Sven Fuchs <svenfuchs@artweb-design.de>
2011-12-08 09:56:27 +0100

    try reverting changes to module/async

--- lib/core_ext/module/async.rb
+++ lib/core_ext/module/async.rb
@@ -1,42 +1,72 @@
 require 'thread'
+require 'singleton'
+require 'delegate'
+require 'monitor'
+require 'core_ext/module/include'
 
 class Async
-  class Queue < Array
-    attr_reader :name
+  class Work < Delegator
+    include MonitorMixin
 
-    def initialize(name)
-      @name = name
+    attr_reader :result
+
+    def initialize(&work)
+      super(work)
+      @work = work
+      @done, @lock = false, new_cond
+    end
+
+    def process
+      synchronize do
+        @result, @done = @work.call, true
+        @lock.signal
+      end
+    end
+
+    def __getobj__
+      synchronize do
+        @lock.wait_while { !@done }
+      end
+      @result
+    end
+
+    def __setobj__(work)
+      @work = work
+    end
+  end
+
+  include Singleton
+
+  include do
+    def initialize
+      @queue = Queue.new
       Thread.new { loop { work } }
     end
 
     def work
-      block = pop
+      block = @queue.pop
       block.call if block
     rescue Exception => e
       puts e.message, e.backtrace
     end
-  end
 
-  class << self
-    def run(name = nil, &block)
-      queue(name) << block
-    end
-
-    def queue(name)
-      queues[name || :default] ||= Queue.new(name)
-    end
-
-    def queues
-      @queues ||= {}
+    def run(&block)
+      @queue.push block
+      @queue.size
     end
   end
 end
 
 Module.class.class_eval do
-  def async(name, options = {})
-    method = instance_method(name)
-    define_method(name) do |*args, &block|
-      Async.run(options[:queue]) { method.bind(self).call(*args, &block) }
+  def async(*names)
+    names.each do |name|
+      method = instance_method(name)
+      define_method(name) do |*args, &block|
+        work = Async::Work.new { method.bind(self).call(*args, &block) }
+        Async.instance.run { work.process }
+        work.result
+      end
     end
   end
 end
+
